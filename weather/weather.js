c4e4d08af2b4416872bb8b7dfdb1cc7ee2cbce08
zcasti1 <78125982+zcasti1@users.noreply.github.com>
2021-04-08 15:57:01 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -10,5 +10,14 @@ var radarDisplayOptions = {
 var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
 var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
 $.getJSON(weatherAlertsUrl, function(data) {
-  L.geoJSON(data).addTo(map);
+  L.geoJSON(data, {
+  style: function(feature){
+  var alertColor = 'orange';
+  if (feature.properties.severity === 'Severe') alertColor = 'red';
+  return { color: alertColor };
+},
+  onEachFeature: function(feature, layer) {
+  layer.bindPopup(feature.properties.headline);
+}
+}).addTo(map);
 });
